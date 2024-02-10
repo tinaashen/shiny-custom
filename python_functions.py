dbe898fdbe961f2e7a491e9b702f366ca562e99d
Tina Shen <shentina18@gmail.com>
2024-02-10 14:57:36 +1100

    changed function name

--- python_functions.py
+++ python_functions.py
@@ -1,15 +1,6 @@
 import numpy as np
 
-def test_string_function(string):
-	return(string)
-	
-def test_math_function(x,y):
-	return(x+y)
-	
-def test_numpy_function(x,y):
-	return(np.add(x,y))
-
-def cell(filepath, resultspath):
+def main(filepath, resultspath):
   
 # coding: utf-8s
 
